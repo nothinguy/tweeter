0fecfba467165d2faf7c2d100c1ed4c0e60c52b2
nothinguy <80864088+nothinguy@users.noreply.github.com>
2021-06-14 17:06:05 -0400

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyABLh4BA73ExEv8_y9XhulpESgIedN7OKw",
+    authDomain: "projectkweeter.firebaseapp.com",
+    projectId: "projectkweeter",
+    storageBucket: "projectkweeter.appspot.com",
+    messagingSenderId: "897569430824",
+    appId: "1:897569430824:web:ea93d11b3981271fd3041d",
+    measurementId: "G-7PBY4T7E65"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
